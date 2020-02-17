58b6b2e59198c1beebebf09361f936fabf23a38a
Quan Nguyen <nguyenquan263@gmail.com>
2020-02-16 18:11:59 -0600

    Sunday work.

--- main.r
+++ main.r
@@ -28,3 +28,31 @@ for (i in 1:numberMedicalRecords) {
 medicalRecords <- cbind(medicalRecords, cholesterolLevel)
 
 View(medicalRecords)
+
+#Blood Pressure attribute
+bloodPressure <- vector()
+
+for (i in 1:numberMedicalRecords) {
+  currentBloodPressureMMHG <- medicalRecords$Systolic.Blood.Pressure..mmHg.[i]
+  
+  if (currentBloodPressureMMHG < 120) {
+    bloodPressure <- c(bloodPressure, "Normal")
+  } else if (currentBloodPressureMMHG < 130) {
+    bloodPressure <- c(bloodPressure, "Elevated")
+  } else if (currentBloodPressureMMHG < 140) {
+    bloodPressure <- c(bloodPressure, "High Blood Pressure Stage 1")
+  } else {
+    bloodPressure <- c(bloodPressure, "High Blood Pressure Stage 2")
+  }
+}
+
+medicalRecords <- cbind(medicalRecords, bloodPressure)
+View(medicalRecords)
+
+#Histogram Cholesterol Level respect to High Blood Pressure
+install.packages("ggplot2")
+library(ggplot2)
+qplot(medicalRecords$Total.Cholestrol.Level..mg.dL., 
+      geom = "histogram",
+      main = "Histogram for Histogram Cholesterol Level grouped by High Blood Pressure",
+      xlab = "Cholesterol Level")
